0c33ede9fdc41a4221459dee2e357749b3695e97
Gregolly França <gregollyff@hotmail.com>
2018-11-28 01:29:19 +0000

    Primeiros contatos com js e algumas praticas.

---
+++ variaveis/README.md
@@ -0,0 +1,50 @@
+# Javascript
+
+- `var` - Usamos para declar variáveis.
+
+```
+var = nomeVariavel;
+```
+
+Podemos declarar varias variáveis em uma mesma linha. 
+
+-------
+
+```
+var sobrenome = 'frança',
+      cidade = 'tangará';
+```
+---
+No javascript o código são sempre movidos para cima, porém o valor atribuído não é movido.
+
+
+---
+
+```
+var comida;
+console.log(comida);
+comida = 'Pizza';
+console.log(comida);
+ ```
+
+ Também podemos alterar o valor de variáveis declaradas com "var" e "let", mas nunca com "const".
+
+---
+
+```
+ var time = 'vasco';
+ console.log(time);
+ time = 'flamengo';
+ console.log(time);
+
+ let animal = 'dog';
+ console.log(animal);
+ animal = 'cat';
+ console.log(animal);
+
+ const address = 'tijuca';
+ console.log(address);
+ address = 'botafogo';
+ console.log(address);
+ ```
+
---
+++ variaveis/script.js
@@ -0,0 +1,55 @@
+var nome = "Greg";
+var idade = 24;
+
+console.log(nome, idade);
+
+var preco = 25;
+
+var totalComprado = 5;
+
+var totalPreco =  totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = 'frança',
+      cidade = 'tangará';
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+
+console.log(semDefinir);  
+
+//console.log(aindanãodefinie);
+var comida;
+console.log(comida);
+ comida = 'Pizza';
+ console.log(comida);
+/*
+ var time = 'vasco';
+
+ console.log(time);
+ time = 'flamengo';
+ console.log(time);
+
+ let animal = 'dog';
+ console.log(animal);
+ animal = 'cat';
+ console.log(animal);
+
+ const address = 'tijuca';
+ console.log(address);
+ address = 'botafogo';
+ console.log(address);
+*/
+ var name = 'Gregolly';
+ var age = "24";
+ var strogonoff;
+ var strogonoff = "prato";
+ let annimal ;
+ let raca;
+ let game;
+ let jogador;
+ let politico; 
+
+ console.log(name, age, strogonoff, annimal, raca, game, jogador, politico)

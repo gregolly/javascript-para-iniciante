f390e4bb1d11f06affd88a34f27b91e70e1b7d1e
Gregolly França <gregollyff@hotmail.com>
2018-12-06 00:32:16 +0000

    Aula es6 booleans e condicionais

---
+++ booleans-e-condicionais/script.js
@@ -0,0 +1,8 @@
+var possuiGraducao = true;
+var possuiDoutorado = true;
+
+if(possuiGraducao){
+  console.log('É verdadeiro');
+} else if(possuiDoutorado){
+  console.log('Possui doutorado');
+}
--- numeros-e-operadores/script.js
+++ numeros-e-operadores/script.js
@@ -1,2 +1,7 @@
 var age= 24;
 console.log(age);
+
+//retorna NaN
+var expressao1 = 'teste ' / 20;
+console.log(expressao1);
+
